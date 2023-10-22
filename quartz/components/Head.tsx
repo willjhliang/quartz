dd82e574e8571a70841f08403ef0673112ce9b51
Will Liang <willjhliang@gmail.com>
2023-10-22 00:40:51 -0400

    removed quartz images

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -12,8 +12,6 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
     const fontStylePath = joinSegments(baseDir, "static/font/cmun.css")
 
     return (
@@ -23,10 +21,8 @@ export default (() => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
